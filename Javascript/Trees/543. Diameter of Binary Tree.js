7bd53dcfbec9d0b68a69af5660078bc86cc2e4ea
Divyanshu Baranwal <divyanshu9210@gmail.com>
2024-02-02 23:20:56 +0530

    leetcode 543 and GFG Boundary Traversal

--- Javascript/Trees/543. Diameter of Binary Tree.js
+++ Javascript/Trees/543. Diameter of Binary Tree.js
@@ -1,35 +1,69 @@
-function height(root) {
-  let queue = [];
-  let height = 0;
-  if (root) {
-    queue.push(root);
-    queue.push(null);
-    while (queue.length > 0) {
-      // Remove the currentNode from queue
-      let currNode = queue.shift();
-      if (!currNode) {
-        // that means level complete
-        height++;
-        if (queue.length > 0) queue.push(null);
-      } else {
-        // Put left and right of the currNode into queue
-        if (currNode.left) queue.push(currNode.left);
-        if (currNode.right) queue.push(currNode.right);
-      }
-    }
-  }
+const { BinaryTree } = require("./Lecture 1");
 
-  return height;
+// function height(root) {
+//   let queue = [];
+//   let height = 0;
+//   if (root) {
+//     queue.push(root);
+//     queue.push(null);
+//     while (queue.length > 0) {
+//       // Remove the currentNode from queue
+//       let currNode = queue.shift();
+//       if (!currNode) {
+//         // that means level complete
+//         height++;
+//         if (queue.length > 0) queue.push(null);
+//       } else {
+//         // Put left and right of the currNode into queue
+//         if (currNode.left) queue.push(currNode.left);
+//         if (currNode.right) queue.push(currNode.right);
+//       }
+//     }
+//   }
+
+//   return height;
+// }
+
+// var diameterOfBinaryTree = function (root) {
+//   if (!root) {
+//     return 0;
+//   }
+
+//   let option1 = diameterOfBinaryTree(root.left);
+//   let option2 = diameterOfBinaryTree(root.right);
+
+//   let option3 = height(root.left) + height(root.right);
+//   return Math.max(option1, option2, option3);
+// };
+
+// Faster way
+function heightUsingRecursion(root, ans) {
+  if (root == null) return 0;
+
+  let lh = heightUsingRecursion(root.left, ans);
+  let rh = heightUsingRecursion(root.right, ans);
+
+  // Extra piece of code in height function
+  let currD = lh + rh;
+  ans.d = Math.max(ans.d, currD);
+  //////////////////////////////////////////
+
+  return Math.max(lh, rh) + 1;
 }
 
 var diameterOfBinaryTree = function (root) {
   if (!root) {
     return 0;
   }
-
-  let option1 = diameterOfBinaryTree(root.left);
-  let option2 = diameterOfBinaryTree(root.right);
-
-  let option3 = height(root.left) + height(root.right);
-  return Math.max(option1, option2, option3);
+  let ans = {
+    d: 0,
+  };
+  heightUsingRecursion(root, ans);
+  return ans.d;
 };
+
+let Tree = new BinaryTree();
+Tree.insert(2);
+Tree.insert(1);
+
+console.log(diameterOfBinaryTree(Tree.getRoot()));
---
+++ Javascript/Trees/GFG-Boundary Traversal of binary tree.js
@@ -0,0 +1,77 @@
+const { BinaryTree } = require("./Lecture 1");
+
+class Solution {
+  solveLeft(root, result) {
+    if (root == null) return;
+    if (root.left == null && root.right == null) {
+      return;
+    }
+
+    result.push(root.data);
+    if (root.left) {
+      this.solveLeft(root.left, result);
+    } else {
+      this.solveLeft(root.right, result);
+    }
+  }
+
+  solveRight(root, result) {
+    if (root == null) return;
+
+    if (root.left == null && root.right == null) {
+      return;
+    }
+
+    if (root.right) {
+      this.solveRight(root.right, result);
+    } else {
+      this.solveRight(root.left, result);
+    }
+
+    result.push(root.data);
+  }
+
+  solveLeaf(root, result) {
+    if (root == null) return;
+    if (root.left == null && root.right == null) {
+      result.push(root.data);
+      return;
+    }
+
+    // Traverse left and right
+    this.solveLeaf(root.left, result);
+    this.solveLeaf(root.right, result);
+  }
+  boundary(root) {
+    let result = [];
+    if (root == null) return result;
+    result.push(root.data);
+
+    // Solve for left boundary
+    this.solveLeft(root.left, result);
+    this.solveLeaf(root.left, result);
+
+    // Solve for right boundary
+    this.solveLeaf(root.right, result);
+    this.solveRight(root.right, result);
+
+    return result;
+  }
+}
+
+let Tree = new BinaryTree();
+// Tree.insert(6);
+// Tree.insert(10);
+// Tree.insert(15);
+// Tree.insert(7);
+// Tree.insert(3);
+// Tree.insert(4);
+// Tree.insert(5);
+
+Tree.insert(2);
+Tree.insert(1);
+Tree.insert(3);
+
+let sol = new Solution();
+let result = sol.boundary(Tree.getRoot());
+console.log("result", result);
